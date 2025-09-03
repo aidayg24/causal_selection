d9efc5c990ab9fee41425d4821f0941f37f567cd
Aida Rostami <45621047+aidayg24@users.noreply.github.com>
2025-09-03 16:52:20 +0200

    update backend

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,8 +1,7 @@
 export default {
-  experimentId: '9',
-  serverUrl: 'https://causal-selection-experiment.gigalixirapp.com',
-  socketUrl: 'wss://causal-selection-experiment.gigalixirapp.com/socket',
-  // this will be used in prolific mode
+  experimentId: '27',
+  serverUrl: 'https://magpie-cogsciprag.fly.dev',
+  socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',// this will be used in prolific mode
   completionUrl: 'https://...',
   contactEmail: 'aidarostamiyg24@gmail.com',
   // Either 'debug', 'directLink' or 'prolific'
