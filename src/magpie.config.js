39f6362d19b48c5769f6a00972c8c3e3a818a3a1
Aida Rostami <45621047+aidayg24@users.noreply.github.com>
2025-10-08 10:27:52 +0200

    disjunctive run on prolific

---
+++ src/magpie.config.js
@@ -0,0 +1,10 @@
+export default {
+  experimentId: '33',
+  serverUrl: 'https://magpie-cogsciprag.fly.dev',
+  socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',// this will be used in prolific mode
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=C1GMPJQ4',
+  contactEmail: 'aidarostamiyg24@gmail.com',
+  // Either 'debug', 'directLink' or 'prolific'
+  mode: 'prolific',
+  language: 'en'
+};
