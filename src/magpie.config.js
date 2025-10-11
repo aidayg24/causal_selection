43d719302e89d02129a7bc6b06d17de517358bde
Aida Rostami <45621047+aidayg24@users.noreply.github.com>
2025-10-11 10:21:58 +0200

    back to direct link

---
+++ src/magpie.config.js
@@ -0,0 +1,10 @@
+export default {
+  experimentId: '32',
+  serverUrl: 'https://magpie-cogsciprag.fly.dev',
+  socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',// this will be used in prolific mode
+  completionUrl: 'https:...',
+  contactEmail: 'aidarostamiyg24@gmail.com',
+  // Either 'debug', 'directLink' or 'prolific'
+  mode: 'directLink',
+  language: 'en'
+};
